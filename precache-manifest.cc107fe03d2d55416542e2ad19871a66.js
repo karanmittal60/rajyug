5b39d13e520d8a07375765029a6a41d46e685116
karan mittal <mittal.karan60@gmail.com>
2020-05-26 00:43:26 +0530

    Updates

---
+++ precache-manifest.cc107fe03d2d55416542e2ad19871a66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1696f34f41382359b6e33ab8a836340c",
+    "url": "/rajyug/index.html"
+  },
+  {
+    "revision": "c05264e6d0433b5e1452",
+    "url": "/rajyug/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "02047c6a99ba553994b0",
+    "url": "/rajyug/static/css/main.79f13381.chunk.css"
+  },
+  {
+    "revision": "c05264e6d0433b5e1452",
+    "url": "/rajyug/static/js/2.f3e8c2ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rajyug/static/js/2.f3e8c2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02047c6a99ba553994b0",
+    "url": "/rajyug/static/js/main.1f4073c2.chunk.js"
+  },
+  {
+    "revision": "e10c3a145f9306e642d4",
+    "url": "/rajyug/static/js/runtime-main.61cc40ae.js"
+  }
+]);
